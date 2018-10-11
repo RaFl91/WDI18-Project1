1b82107cf7a90029f86064299a95d9c909c0d5a8
RaFl91 <Terrell.Flemings@gmail.com>
2018-10-11 17:43:27 -0400

    Creation: pages and images folder, modify: creation of word list

---
+++ main.js
@@ -0,0 +1,22 @@
+var wordList = [{
+    word: "DIS",
+    hint1: "The infernal city"
+}, {
+    word: "TARTARUS",
+    hint1: "River of blood that boils souls"
+}, {
+    word: "VIRGIL",
+    hint1: "Your underworld tour guide"
+}, {
+    word: "SIN",
+    hint1: "An act of transgression against divine law"
+}, {
+    word: "HELL",
+    hint1: "10 circles of eternal suffering"
+}, {
+    word: "LUCIFER",
+    hint1: `The fallen one, Son of the morning, The crooked serpent, Ib'lis, Frozen in center`
+}, {
+    word: "MALEBRANCHE",
+    hint1: "Winged demons who torment politicians in the fifth Maleboge of the Eighth circle"
+}]
