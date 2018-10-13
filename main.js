bddb505f4e8838e759b6500b4313dd2afb70e8de
RaFl91 <Terrell.Flemings@gmail.com>
2018-10-12 20:40:44 -0400

    Modify: addition of buttons to .js file  BRANCH: initial switch to ternary operators

--- main.js
+++ main.js
@@ -26,4 +26,49 @@ var wordList = [{
 const displayField = document.querySelector("#inputField")
 const guessField = document.querySelector(".guessField")
 const attemptField = document.querySelector("#livesField")
-const soundWin = new Audio("./")
+const soundWin = new Audio("/audio/audioSuccess.mp3")
+const soundFail = new Audio("/audio/audioFail.mp3")
+let lives = 0
+var alephArr = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+var wrongAleph = []
+var word
+var wordArr
+var letterSpace = " "
+var guessPass
+ 
+//  === Keyboard ===
+var A = document.getElementById("A")
+var B = document.getElementById("B")
+var C = document.getElementById("C")
+var D = document.getElementById("D")
+var E = document.getElementById("E")
+var F = document.getElementById("F")
+var G = document.getElementById("G")
+var H = document.getElementById("H")
+var I = document.getElementById("I")
+var J = document.getElementById("J")
+var K = document.getElementById("K")
+var L = document.getElementById("L")
+var M = document.getElementById("M")
+var N = document.getElementById("N")
+var O = document.getElementById("O")
+var P = document.getElementById("P")
+var Q = document.getElementById("Q")
+var R = document.getElementById("R")
+var S = document.getElementById("S")
+var T = document.getElementById("T")
+var U = document.getElementById("U")
+var V = document.getElementById("V")
+var W = document.getElementById("W")
+var X = document.getElementById("X")
+var Y = document.getElementById("Y")
+var Z = document.getElementById("Z")
+
+function game(){
+    wordObj = wordList[Math.floor(Math.random()* wordList.length)]
+    word = wordObj.word
+    wordArr = word.split('')
+    wordBoxLength(wordArr)
+    guessPass =wordArr.length 
+    var hint1 = document.getElementById('hint1')
+}
